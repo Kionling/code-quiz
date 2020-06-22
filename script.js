50818420877c975796aba5fe11bca5e891a2e583
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-06-21 23:40:35 -0700

    Generated countdown timer

--- script.js
+++ script.js
@@ -5,9 +5,28 @@ var eb = document.getElementById("lopB");
 var ec = document.getElementById("lopC");
 var ed = document.getElementById("lopD");
 var eq = document.getElementById("questionEL");
+var actualQ = undefined;
+var nxtQ = 1;
+var arrQ = [];
+var score = 0;
+var count = 5;
+var counter = setInterval(timer, 1000);
 
+function timer()
+{
+  count=count-1;
+  if (count < 0)
+  {
+     clearInterval(counter);
+     return;
+  }
+  if (count === 0){
+      document.getElementById("gameOver").innerHTML = "Sorry you have run out of time";
+      document.getElementById("time").style.color = "white";
+  }
 
-
+ document.getElementById("timer").innerHTML=count + " secs"; 
+}
 
 
 
@@ -29,7 +48,7 @@ q1.b = "Prevents HTML elements from deleting when accidentally hitting backspace
 q1.c ="Prevents the cat from losing weight";
 q1.d = "Prevents bootstrap columns from resetting ";
 q1.answer = "a";
-q1.wrongAnswer = ["b", "c", "d"]
+arrQ.push(q1);
 
 q2 = new qst();
 q2.question = "What is Javascript?";
@@ -38,30 +57,37 @@ q2.b = "describes how HTML elements are to be displayed on screen, paper, or in 
 q2.c ="an interpreted, high-level, general-purpose programming language.";
 q2.d = "a general-purpose programming language created by Bjarne Stroustrup";
 q2.answer = "a";
+arrQ.push(q2);
 
 q3 = new qst();
 q3.question = "What are APIs user for?";
-q3.a ="";
-q3.b = "blue";
-q3.c ="An API allows me to ";
-q3.d = "an API specifies how software components should interact.";
+q3.a ="An API allows me dictates how large the cache files can be";
+q3.b = "An API allows me to make my browser load videos faster";
+q3.c ="An API allows me to change colors in a div";
+q3.d = "An API specifies how software components should interact";
 q3.answer = "d";
+arrQ.push(q3);
 
 q4 = new qst();
-q4.question = "This is my favorite color";
-q4.a ="Red";
-q4.b = "blue";
-q4.c ="baby blue";
-q4.d = "lightcoral";
+q4.question = "Who created Javascript?";
+q4.a ="Mitchell Baker";
+q4.b = "Brendan Eich";
+q4.c ="Gottfried Leibniz";
+q4.d = "Bruce Wayne";
 q4.answer = "b";
+arrQ.push(q4);
 
 q5 = new qst();
-q5.question = "";
-q5.a ="Red";
-q5.b = "blue";
-q5.c ="baby blue";
-q5.d = "lightcoral";
+q5.question = "Who created APIs";
+q5.a ="Roy Fielding";
+q5.b = "Steph Curry";
+q5.c ="Bill Gates";
+q5.d = "Bjarne Stroustrup";
 q5.answer = "a";
+arrQ.push(q5);
+
+function scores(){
+}
 
 
 
@@ -70,11 +96,37 @@ q5.answer = "a";
 
 
 function givenAnswer(){
+    nxtQ++;
+    if (nxtQ < 6){
+        setQuestion(arrQ[nxtQ]);
+    }
+    else {
+
+
+    }
+    // if (opc === q1.answer){
+    //     alert("Correct!");
+    //     actualQ.userAnswer = opc;
+    // }
+    // else {
+    //     alert("Wrong!")
+    // }
+}
+
+function begin(){
+    setQuestion(arrQ[nxtQ]);
+    document.getElementById("")
 
 
 }
 
-function begin(){
+function setQuestion(q){
+    eq.innerHTML = q.question;
+    ea.innerHTML = q.a;
+    eb.innerHTML = q.b;
+    ec.innerHTML = q.c;
+    ed.innerHTML = q.d;
+    actualQ = q;
 
 }
 
@@ -83,5 +135,7 @@ function highscores(){
 
 function setOpc(op) {
     opc = op;
-    alert(opc);
+    actualQ.userAnswer = opc;
+    // alert(opc);
+    
 }
