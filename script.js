a95c7c86acfd7fdcead68a654c36cd6da141e1b2
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-06-23 09:47:23 -0700

    Generating error/time calculations for wrong answers

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var ec = document.getElementById("lopC");
 var ed = document.getElementById("lopD");
 var eq = document.getElementById("questionEL");
 var actualQ = undefined;
-var nxtQ = 1;
+var nxtQ = 0;
 var arrQ = [];
 var score = 0;
 var count = 5;
@@ -14,18 +14,14 @@ var start = document.getElementById("start-btn");
 var interval;
 var timerDisplay = document.getElementById("timer");
 var totalSeconds = 60;
+// var gameEndDisplay = document.getElementById("gameOver");
+// var startOver = document.getElementById("end-btn");
+// var startOver = document.getElementById("end-btn").style.display = "none"
+// var saveBtn = document.getElementById("save-btn").style.display = "none";
+var endTitle = document.getElementById("end-container");
+var gameEndDisplay = document.getElementById("gameOver")
 
 
-// var interval = setInterval(function(){
-//     document.getElementById('time').innerHTML=count;
-//     count--;
-//     if (count === 0){
-//       clearInterval(interval);
-//       document.getElementById('time').innerHTML='Done';
-//       // or...
-//       alert("You're out of time!");
-//     }
-//   }, 1000);
 
 function timer(){
     start = document.getElementById("start-btn").style.display = "none";
@@ -36,6 +32,13 @@ function timer(){
     }
     else {
         clearInterval(interval);
+        var quizEL = document.getElementById("answer-btns").style.display = "none";
+        gameEndDisplay.innerHTML = "Sorry you ran out of time.";
+        endTitle.setAttribute("style", "display: block;");
+        
+        
+
+        
     }
     },1000);
 }
@@ -70,71 +73,68 @@ function qst(){
     this.answer = "";
     this.question ="";
     this.userAnswer = "";
+    this.result = 0;
 }
 // Quiz preparation
-q1 = new qst();
-q1.question = "";
-q1.a ="";
-q1.b = "";
-q1.c ="";
-q1.d = "";
-q1.answer = "";
-arrQ.push(q1);
+// quiz placeholder
 
+
+
+
+q1 = new qst();
+q1.question = "What is Javascript?";
+q1.a ="a scripting or programming language that allows you to implement complex features on web pages.";
+q1.b = "describes how HTML elements are to be displayed on screen, paper, or in other media";
+q1.c ="an interpreted, high-level, general-purpose programming language.";
+q1.d = "a general-purpose programming language created by Bjarne Stroustrup";
+q1.answer = "a";
+arrQ.push(q1);
+actualQ = q1;
 
 
 q2 = new qst();
-q2.question = "What is Javascript?";
-q2.a ="a scripting or programming language that allows you to implement complex features on web pages.";
-q2.b = "describes how HTML elements are to be displayed on screen, paper, or in other media";
-q2.c ="an interpreted, high-level, general-purpose programming language.";
-q2.d = "a general-purpose programming language created by Bjarne Stroustrup";
-q2.answer = "a";
+q2.question = "What are APIs user for?";
+q2.a ="An API allows me dictates how large the cache files can be";
+q2.b = "An API allows me to make my browser load videos faster";
+q2.c ="An API allows me to change colors in a div";
+q2.d = "An API specifies how software components should interact";
+q2.answer = "d";
 arrQ.push(q2);
 
 
+
 q3 = new qst();
-q3.question = "What are APIs user for?";
-q3.a ="An API allows me dictates how large the cache files can be";
-q3.b = "An API allows me to make my browser load videos faster";
-q3.c ="An API allows me to change colors in a div";
-q3.d = "An API specifies how software components should interact";
-q3.answer = "d";
+q3.question = "Who created Javascript?";
+q3.a ="Mitchell Baker";
+q3.b = "Brendan Eich";
+q3.c ="Gottfried Leibniz";
+q3.d = "Bruce Wayne";
+q3.answer = "b";
 arrQ.push(q3);
 
 
 
 q4 = new qst();
-q4.question = "Who created Javascript?";
-q4.a ="Mitchell Baker";
-q4.b = "Brendan Eich";
-q4.c ="Gottfried Leibniz";
-q4.d = "Bruce Wayne";
-q4.answer = "b";
+q4.question = "Who created APIs";
+q4.a ="Roy Fielding";
+q4.b = "Steph Curry";
+q4.c ="Bill Gates";
+q4.d = "Bjarne Stroustrup";
+q4.answer = "a";
 arrQ.push(q4);
 
-
-
 q5 = new qst();
-q5.question = "Who created APIs";
-q5.a ="Roy Fielding";
-q5.b = "Steph Curry";
-q5.c ="Bill Gates";
-q5.d = "Bjarne Stroustrup";
+q5.question = "What does event.preventDefault() do?";
+q5.a ="stops the default action of an element from happening.";
+q5.b = "Prevents HTML elements from deleting when accidentally hitting backspace.";
+q5.c ="Prevents the cat from losing weight";
+q5.d = "Prevents bootstrap columns from resetting ";
 q5.answer = "a";
 arrQ.push(q5);
 
-q6 = new qst();
-q6.question = "What does event.preventDefault() do?";
-q6.a ="stops the default action of an element from happening.";
-q6.b = "Prevents HTML elements from deleting when accidentally hitting backspace.";
-q6.c ="Prevents the cat from losing weight";
-q6.d = "Prevents bootstrap columns from resetting ";
-q6.answer = "a";
-arrQ.push(q6);
-
 
 function scores(){
+
 }
 
 
@@ -144,30 +144,46 @@ function scores(){
 
 
 function givenAnswer(){
-    nxtQ++;
-    if (nxtQ < 7){
+    
+    if (nxtQ < 6){
         setQuestion(arrQ[nxtQ]);
-    }
-    //totalSeconds--;
-    //timerDisplay.textContent = totalSeconds + " seconds left";
-    if ( opc === q1.answer && q2.answer && q3.answer && q4.answer && q5.answer){
-        totalSeconds += 5;
+    actualQ = arrQ[nxtQ];
+
+    if ( actualQ.answer != actualQ.userAnswer){
+        totalSeconds -= 5;
+
+         if(score > -1){
+            score -= 1;
+        }
     }
     else {
-        totalSeconds -= 5;
+        score++;
     }
-    }
-    // if (opc === q1.answer){
-    //     alert("Correct!");
-    //     actualQ.userAnswer = opc;
-    // }
-    // else {
-    //     alert("Wrong!")
-    // }
 
+    // alert("user answer" + actualQ.userAnswer + "\n correct answer" + actualQ.answer);
+
+}
+
+
+    else {
+        var r = 0, w = 0;
+        for(var i = 0; i < arrQ.length; i++){
+            r += arrQ[i].result * 1;
+            
+        }
+        
+        alert("right answer = " + r + "\n wrong answers = " + arrQ.length - r);
+    }
+    
+    nxtQ++;
+    actualQ = arrQ[nxtQ];
+}
 
 function begin(){
     setQuestion(arrQ[nxtQ]);
+    var titleEl = document.getElementById("titles").style.display = "none";
+
+
 
 }
 
@@ -187,9 +203,9 @@ function highscores(){
 function setOpc(op) {
     opc = op;
     actualQ.userAnswer = opc;
+   
     // alert(opc);
     
 }
 
 start.addEventListener("click", timer);
-console.log(q1.question);
